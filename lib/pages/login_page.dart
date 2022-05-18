a02661906c7f30deec9a459551d34a527072fed1
Andrew Bell <fiixed@gmail.com>
2022-05-18 09:33:35 -0400

    email/password fields with validation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,6 +9,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   double? _deviceHeight, _deviceWidth;
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  String? _email;
+  String? _password;
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -24,6 +28,7 @@ class _LoginPageState extends State<LoginPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleWidget(),
+                _loginForm(),
                 _loginButton(),
               ],
             ),
@@ -38,6 +43,57 @@ class _LoginPageState extends State<LoginPage> {
       'Finstagram',
       style: TextStyle(
           color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _deviceHeight! * 0.20,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _emailTextField(),
+            _passwordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: "Email..."),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+          RegExp(
+              r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"),
+        );
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(hintText: "Password..."),
+      onSaved: (_value) {
+        setState(() {
+          _password = _value;
+        });
+      },
+      validator: (_value) => _value!.length > 6
+          ? null
+          : "Please enter a password greater than 6 characters.",
     );
   }
 
